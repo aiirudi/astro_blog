c7810ad4c5959b5116379d995a108787aed792a4
aiirudi <121801588+aiirudi@users.noreply.github.com>
2025-12-18 17:00:26 +0800

    Update profile name from 'Lorem Ipsum' to 'Flora'

--- src/config.ts
+++ src/config.ts
@@ -54,7 +54,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
+	name: "Flora",
 	bio: "you should try ~",
 	links: [
 		{
